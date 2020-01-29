553cd2638314e9bb1494921f3e578884d009f250
Joshua Marsh <joshmrsh@amazon.com>
2020-01-29 11:15:39 -0600

    Added account_pool_metrics queue

---
+++ modules/account_pool_metrics.tf
@@ -0,0 +1,54 @@
+/*
+Keep a table of basic account pool metrics up to date using events from the accounts queue
+*/
+resource "aws_dynamodb_table" "metrics" {
+  name             = "Metrics${local.table_suffix}"
+  read_capacity    = 5
+  write_capacity   = 5
+  hash_key         = "Id"
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+
+  server_side_encryption {
+    enabled = true
+  }
+
+  attribute {
+    name = "Id"
+    type = "S"
+  }
+
+  tags = var.global_tags
+}
+
+resource "aws_sqs_queue" "account_pool_metrics" {
+  name = "account_pool_metrics-${var.namespace}"
+  visibility_timeout_seconds = 3600
+  tags = var.global_tags
+}
+
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  topic_arn = aws_sns_topic.accounts.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.account_pool_metrics.arn
+}
+
+module "update_account_pool_metrics_lambda" {
+  source          = "./lambda"
+  name            = "update_account_pool_metrics-${var.namespace}"
+  namespace       = var.namespace
+  description     = "Updated AccountPoolMetrics DB in response to Accounts DB changes"
+  global_tags     = var.global_tags
+  handler         = "update_account_pool_metrics"
+  alarm_topic_arn = aws_sns_topic.alarms_topic.arn
+
+  environment = {
+    AWS_CURRENT_REGION       = var.aws_region
+    ACCOUNT_DB               = aws_dynamodb_table.metrics.id
+  }
+}
+
+resource "aws_lambda_event_source_mapping" "update_account_pool_metrics_from_accounts_sqs" {
+  event_source_arn  = aws_sqs_queue.account_pool_metrics.arn
+  function_name     = module.update_account_pool_metrics_lambda.name
+}
--- modules/dynamodb.tf
+++ modules/dynamodb.tf
@@ -50,6 +50,35 @@ resource "aws_dynamodb_table" "accounts" {
   */
 }
 
+resource "aws_sns_topic" "accounts" {
+  name = "accounts-${var.namespace}"
+  tags = var.global_tags
+}
+
+module "publish_account_events_lambda" {
+  source          = "./lambda"
+  name            = "publish_account_events-${var.namespace}"
+  namespace       = var.namespace
+  description     = "Updated AccountPoolMetrics DB in response to Accounts DB changes"
+  global_tags     = var.global_tags
+  handler         = "publish_account_events"
+  alarm_topic_arn = aws_sns_topic.alarms_topic.arn
+
+  environment = {
+    AWS_CURRENT_REGION       = var.aws_region
+    ACCOUNT_DB               = aws_dynamodb_table.accounts.id
+    ACCOUNT_TOPIC_ARN        = aws_sns_topic.accounts.arn
+  }
+}
+
+resource "aws_lambda_event_source_mapping" "publish_account_events_from_dynamo_db" {
+  event_source_arn  = aws_dynamodb_table.accounts.stream_arn
+  function_name     = module.publish_account_events_lambda.name
+  batch_size        = 1
+  starting_position = "LATEST"
+}
+
+
 resource "aws_dynamodb_table" "leases" {
   name             = "Leases${local.table_suffix}"
   read_capacity    = 5
